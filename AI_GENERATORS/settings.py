419e64c88a8226f2a62ccd57a31164f2225ad80b
Vivek <185585908+vivektyagi05@users.noreply.github.com>
2026-04-05 11:59:29 +0530

    Update README and fix OTP email notice

--- AI_GENERATORS/settings.py
+++ AI_GENERATORS/settings.py
@@ -18,10 +18,10 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
--- accounts/email_service.py
+++ accounts/email_service.py
@@ -1,29 +1,4 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
-import requests
-from django.conf import settings
-
-def send_email_async(subject, message, to_email):
-    print("🔥 EMAIL FUNCTION STARTED")
-
-    try:
-        res = requests.post(
-            "https://api.resend.com/emails",
-            headers={
-                "Authorization": f"Bearer {settings.RESEND_API_KEY}",
-                "Content-Type": "application/json"
-            },
-            json={
-                "from": "onboarding@resend.dev",
-                "to": [to_email],
-                "subject": subject,
-                "html": f"<p>{message}</p>"
-            },
-            timeout=10
-        )
-
-        print("✅ EMAIL SENT:", res.text)
-
-    except Exception as e:
-        print("❌ EMAIL ERROR:", e)
+z
